83b114cdff99d4fabbd79309f253efa3999832cb
ml5885 <44534695+ml5885@users.noreply.github.com>
2024-07-05 23:27:56 -0700

    fixed UI bugs

--- src/components/ColorPalette.tsx
+++ src/components/ColorPalette.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, MouseEvent } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import placeholderSrc from "../assets/placeholder.png";
 
@@ -9,86 +9,94 @@ interface ColorPaletteProps {
 	colors: Color[];
 }
 
-const ColorPalette: React.FC<ColorPaletteProps> = (props) => {
-	const { colors, image } = props;
-	const [hoveredColor, setHoveredColor] = useState<Color | null>(null);
-	const [isExpanded, setIsExpanded] = useState(false);
-	const [copiedColor, setCopiedColor] = useState<string | null>(null);
+const ColorPalette: React.FC<ColorPaletteProps> = React.memo(
+	({ colors, image }) => {
+		const [hoveredColor, setHoveredColor] = useState<Color | null>(null);
+		const [isExpanded, setIsExpanded] = useState<boolean>(false);
+		const [copiedColor, setCopiedColor] = useState<Color | null>(null);
+		const [key, setKey] = useState<number>(0); // Add this line
 
-	const handleColorHover = (color: Color) => {
-		setHoveredColor(color);
-	};
+		const handleColorHover = useCallback((color: Color | null) => {
+			setHoveredColor(color);
+		}, []);
 
-	const handleColorClick = (color: Color) => {
-		navigator.clipboard.writeText(color);
-	    setCopiedColor(color);
+		const handleColorClick = useCallback((color: Color) => {
+			navigator.clipboard.writeText(color);
+			setCopiedColor(null);
+			setTimeout(() => {
+				setCopiedColor(color);
+				setKey((prevKey) => prevKey + 1);
+			}, 10);
+		}, []);
 
-	    setTimeout(() => {
-	      setCopiedColor(null);
-	    }, 2000);
-	};
+		const handleImageClick = useCallback(() => {
+			setIsExpanded(true);
+		}, []);
 
-	const handleImageClick = () => {
-		setIsExpanded(true);
-	};
+		const handleClose = useCallback(() => {
+			setIsExpanded(false);
+			setHoveredColor(null);
+		}, []);
 
-	const handleClose = () => {
-		setIsExpanded(false);
-		setHoveredColor(null);
-	};
+		const handleOverlayClick = useCallback(
+			(e: MouseEvent<HTMLDivElement>) => {
+				if (
+					isExpanded &&
+					e.target instanceof HTMLElement &&
+					!e.target.closest(".modal-content")
+				) {
+					handleClose();
+				}
+			},
+			[isExpanded, handleClose]
+		);
 
-	const handleOverlayClick = (e: React.MouseEvent) => {
-		if (
-			isExpanded &&
-			e.target instanceof HTMLElement &&
-			!e.target.closest(".modal-content")
-		) {
-			handleClose();
-		}
-	};
-
-	return (
-		<div className="color-palette-wrapper">
-			<div className="color-palette-image" onClick={handleImageClick}>
-				<LazyLoadImage
-					src={image}
-					alt="image"
-					placeholder={
-						<span>
-							<img src={placeholderSrc} alt="placeholder iamge"></img>
-						</span>
-					}
-				/>
-			</div>
-			<div className="color-palette">
-				{colors.map((color, index) => (
-					<div
-						className="color-circle"
-						style={{ background: color }}
-						onMouseEnter={() => handleColorHover(color)}
-						onMouseLeave={() => handleColorHover("")}
-						onClick={() => handleColorClick(color)}
-					>
-						{hoveredColor === color && <div className="tooltip">{color}</div>}
+		return (
+			<div className="color-palette-wrapper">
+				<div className="color-palette-image" onClick={handleImageClick}>
+					<LazyLoadImage
+						src={image}
+						alt="Color palette inspiration"
+						placeholder={<img src={placeholderSrc} alt="Loading placeholder" />}
+						effect="blur"
+						threshold={100}
+					/>
+				</div>
+				<div className="color-palette">
+					{colors.map((color, index) => (
+						<div
+							key={index}
+							className="color-circle"
+							style={{ background: color }}
+							onMouseEnter={() => handleColorHover(color)}
+							onMouseLeave={() => handleColorHover(null)}
+							onClick={() => handleColorClick(color)}
+						>
+							{hoveredColor === color && <div className="tooltip">{color}</div>}
+						</div>
+					))}
+				</div>
+				{copiedColor && (
+					<div key={key} className="color-copied-popup">
+						Copied {copiedColor}
 					</div>
-				))}
-			</div>
-	      	{copiedColor && 
-      		<div className="color-copied-popup">
-      			Copied {copiedColor}
-  			</div>}
-
-			{isExpanded && (
-				<div className="modal" onClick={handleOverlayClick}>
-					<div className="modal-content">
-						<div className="modal-image">
-							<LazyLoadImage src={image} alt="image" />;
+				)}
+				{isExpanded && (
+					<div className="modal" onClick={handleOverlayClick}>
+						<div className="modal-content">
+							<div className="modal-image">
+								<LazyLoadImage
+									src={image}
+									alt="Expanded color palette inspiration"
+									effect="blur"
+								/>
+							</div>
 						</div>
 					</div>
-				</div>
-			)}
-		</div>
-	);
-};
+				)}
+			</div>
+		);
+	}
+);
 
 export default ColorPalette;
